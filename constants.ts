b7c825527cc4a2fab1817e6ff917ab2160c11dca
Rami Elwan <ramielwan48@gmail.com>
2022-07-21 13:34:00 +0300

    added constants

---
+++ constants.ts
@@ -0,0 +1,5 @@
+import { Dimensions } from "react-native";
+
+export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+export const CENTER = { x: SCREEN_WIDTH / 2, y: SCREEN_HEIGHT / 2 };
